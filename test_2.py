77de463650a09ce7bf258e5d8a464af5c0b0b576
Sayan Maity <sayanmaity8004@gmail.com>
2024-10-20 17:50:25 +0530

    python beginner done

---
+++ test_2.py
@@ -0,0 +1,20 @@
+# tup1 = (1,"fhsuhd",True)
+# tup2 = (2,False)
+# print(tup1+tup2)
+# import numpy as np 
+# n1 = np.zeros((5,5))
+# print(n1)
+# import pandas as pd
+# df = pd.DataFrame()
+# list1=[1,2,3,4]
+# list2=[4,5,6,7]
+# df["list1"]=list1
+# df["list2"]=list2
+# print(df)
+# dict1={"id":[1,2]}
+# dict1=pd.DataFrame(dict1)
+# list11=pd.DataFrame(list1)
+# i=["a","b","c","d"]
+# list11.index=i
+# list11.loc["c"]
+
