ca9af0361ed7bd93fcd1496855309b474723ace7
Sayan Maity <sayanmaity8004@gmail.com>
2024-10-22 19:06:32 +0530

    find out common letters first question python

--- test_2.py
+++ test_2.py
@@ -18,3 +18,15 @@
 # list11.index=i
 # list11.loc["c"]
 
+
+def frequency_words(x):
+    li=x.split()
+    print(li)
+    d={}
+    for i in li:
+        if i not in d.keys():
+            d[i]=0
+        d[i]=d[i]+1
+    print(d)
+    
+frequency_words("Hello, My name is Sayan , Sayan .")
